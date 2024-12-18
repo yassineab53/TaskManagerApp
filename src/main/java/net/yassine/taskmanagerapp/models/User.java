42280ce617a334800a21ef9bf9d5e693d3b54ce4
yassine4real <yssnabbou2002@gmail.com>
2024-12-19 00:26:24 +0100

    add User class

---
+++ src/main/java/net/yassine/taskmanagerapp/models/User.java
@@ -0,0 +1,21 @@
+package net.yassine.taskmanagerapp.models;
+
+
+import jakarta.persistence.*;
+
+
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+
+
+}
