f7221f1c152f0fd4ba75670bc17999d5f90f17a0
yassine4real <yssnabbou2002@gmail.com>
2024-12-19 00:27:00 +0100

    add Task class

---
+++ src/main/java/net/yassine/taskmanagerapp/models/Task.java
@@ -0,0 +1,16 @@
+package net.yassine.taskmanagerapp.models;
+
+
+import jakarta.persistence.*;
+
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
