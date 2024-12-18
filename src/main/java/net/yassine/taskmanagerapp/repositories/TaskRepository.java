606ac31ee9d977357f98064ea6b53eaea7cd0d13
yassine4real <yssnabbou2002@gmail.com>
2024-12-19 00:36:03 +0100

    add interfaces TaskRepository and UserRepository for managing persisting of object and operation with db using spring data

---
+++ src/main/java/net/yassine/taskmanagerapp/repositories/TaskRepository.java
@@ -0,0 +1,11 @@
+package net.yassine.taskmanagerapp.repositories;
+
+
+import net.yassine.taskmanagerapp.models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByUserUsername(String username);
+}
