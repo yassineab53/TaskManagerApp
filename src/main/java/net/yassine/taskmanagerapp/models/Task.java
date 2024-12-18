d8eb6aa70bc689ac2da42c89a890ce2bc68017cd
yassine4real <yssnabbou2002@gmail.com>
2024-12-19 00:47:19 +0100

    add lombok to classes

--- src/main/java/net/yassine/taskmanagerapp/models/Task.java
+++ src/main/java/net/yassine/taskmanagerapp/models/Task.java
@@ -2,7 +2,13 @@ package net.yassine.taskmanagerapp.models;
 
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+
+@Entity
+@Getter
+@Setter
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/net/yassine/taskmanagerapp/models/User.java
+++ src/main/java/net/yassine/taskmanagerapp/models/User.java
@@ -2,11 +2,14 @@ package net.yassine.taskmanagerapp.models;
 
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
+@Entity
+@Data
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
